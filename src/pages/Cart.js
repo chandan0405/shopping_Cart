dfc8cf8e0944158e9117aafd04cde2d41f06f28b
chandan kumar <chandan04051999@gmail.com>
2023-11-18 14:37:27 +0530

    cleaned code

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,25 +7,7 @@ import { Link } from "react-router-dom";
 function Cart() {
   const {total, cartList}=useCart();
   UseTitles("Cart");
-  console.log(cartList);
-  const productArray = [
-    {
-      id: 1,
-      imageSrc:
-        "https://www.reliancedigital.in/medias/Xiaomi-Basic-EP-492520057-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w0NDg0N3xpbWFnZS9qcGVnfGltYWdlcy9oYzQvaDFiLzk5ODE3Mzk2OTYxNTguanBnfGYxNThlMWJlNDFjODFkNzkxNTU2MWU4NTczODQ5ZjA0NzZjZjM2MDNmZjhjN2UxMDRkYjE0ZmMxOGIyM2YxN2M",
-      alt: "wired oneplus ",
-      productName: "OnePlus Nord E103A Wired Earphone",
-      price: "$15",
-    },
-    {
-      id: 2,
-      imageSrc:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvypLIybiGn5rcbw26npKvBwuxg3H_jZYkGA&usqp=CAU",
-      alt: "boat headphone",
-      productName: "boAt Bassheads 950v2",
-      price: "$15",
-    },
-  ];
+  // console.log(cartList);
   return (
     <main>
       <section className="cart">
